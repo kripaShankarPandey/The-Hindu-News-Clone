638831eb295340092d72f876131da8f301a47d96
kripa Shankar Pandey <kripapandey302@gmail.com>
2022-08-25 23:43:50 +0530

    footer page completed

---
+++ script/header.js
@@ -0,0 +1,16 @@
+
+    var a = document.querySelector("#date")
+    
+    var Days = ['Sunday', 'Monday', 'Tuesday',
+            'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    var Months = ['January', 'February', 'March', 'April',
+            'May', 'June', 'July', 'August', 'September',
+            'October', 'November', 'December'];
+    
+    var currentDay = new Date();
+    var day = Days[currentDay.getDay()];
+    var month = Months[currentDay.getMonth()];
+    var year=currentDay.getFullYear();
+    var date=currentDay.getDate();
+    a.innerText =day+" "+date+" "+month+" "+year;
